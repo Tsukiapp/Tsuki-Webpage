6211d546d8321d9ee6721d16f112dcb3e06e1816
Víctor Leal <victor.leale2003@gmail.com>
2021-07-31 16:15:53 -0400

    Added html structure and css styling

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const urls = {
+    anime: `http://localhost:5000/anime/search?keyword=$&type=anime`,
+    news: `http://localhost:5000/anime/news`,
+    detailedNews: `http://localhost:5000/anime/news/details?id={anime_id}`,
+    season: `http://localhost:5000/anime/season`,
+    top: `http://localhost:5000/anime/top`
+}
+
 function getAnimeInfo(animeName) {
     return new Promise((resolve, reject) => {
-        resolve(fetch(`http://localhost:5000/anime/news`, {mode: "no-cors"}))
+        resolve(fetch(`http://localhost:5000/anime/search?keyword=${animeName}&type=anime`, {mode: "no-cors"}))
         reject("ERROR")
     })
 }
@@ -9,11 +17,13 @@ async function displayAnimeInfo(userInput) {
     try {
         const anime = await getAnimeInfo(userInput)
         const processedAnime = await anime.json()
-        console.log(processedAnime[0].newsThumbnail)
-        document.getElementById("anime__card-image").src = processedAnime[0].newsThumbnail
+        console.log(processedAnime)
     } catch(error) {
         console.log(error)
     }
 }
+let input = document.getElementById("searchInput")
 
-displayAnimeInfo("death note")
+input.addEventListener("keyup", () => {
+    displayAnimeInfo(input.value)
+})
